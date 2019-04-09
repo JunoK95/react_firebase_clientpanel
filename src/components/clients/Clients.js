fbd1870873dc6f4be07e5e02a9bc6027de292908
junok <junokimemail@gmail.com>
2019-04-09 01:06:24 -0600

    finished client component

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,24 +1,33 @@
 import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import {compose} from 'redux'
+import {connect} from 'react-redux'
+import {firestoreConnect} from 'react-redux-firebase'
+import LoadingGif from '../layout/LoadingGif'
 
 class Clients extends Component {
+    state = {
+        totalOwed: '0'
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        const {clients}= props
+        if(clients){
+            const total = clients.reduce((total, client) => {
+                return total + parseFloat(client.balance.toString())
+            }, 0)
+
+            return {totalOwed: total}
+        }
+        else{
+            return null;
+        }
+    }
+
     render() {
-        const clients = [{
-            id: '1',
-            firstName: 'Adam',
-            lastName: 'Abbot',
-            email: 'aaa@gmail.com',
-            phone: '111-111-1111',
-            balance: '30'
-        },
-        {
-            id: '2',
-            firstName: 'Bob',
-            lastName: 'Barley',
-            email: 'bbb@gmail.com',
-            phone: '222-222-2222',
-            balance: '40'
-        }]
+        const {clients} = this.props;
+        const {totalOwed} = this.state;
 
         if (clients){
             return (
@@ -30,7 +39,15 @@ class Clients extends Component {
                                 <i className="fas fa-users" /> Clients{' '}
                             </h2>
                         </div>
-                        <div className="col-md-6" />
+                        <div className="col-md-6">
+                            <h5 className="text-right text-secondary">
+                                Total Owed{' '}
+                                <span className="text-primary">
+                                    ${parseFloat(totalOwed).toFixed(2)}
+                                </span>
+                            </h5>
+
+                        </div>
                     </div>
 
                     <table className="table table-striped">
@@ -62,10 +79,22 @@ class Clients extends Component {
               )
         }
         else{
-            return(<h1>loading...</h1>)
+            return(<LoadingGif />)
         }
     }
   }
+
+  Clients.propTypes ={
+      firestore:  PropTypes.object.isRequired,
+      clients: PropTypes.array
+  }
   
-  export default Clients;
+  // gets clients from firestore and will put it into state.firestore.ordered.
+  //then assign clients in this component (mapPropsToState) to be able to use as a Prop
+  export default compose(
+      firestoreConnect([{ collection: 'clients'}]),
+      connect((state,props) => ({
+          clients: state.firestore.ordered.clients
+      }))
+  )(Clients);
   
